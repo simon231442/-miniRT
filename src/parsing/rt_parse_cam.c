7fa2d631362210d7f7ef38c15722ec26713d7908
Simon Renaud <srenaud@c2r5s6.42lausanne.ch>
2026-01-17 15:24:00 +0100

    simon : is in range, bientot pres a integre etre

--- src/parsing/rt_parse_cam.c
+++ src/parsing/rt_parse_cam.c
@@ -6,7 +6,7 @@
 /*   By: srenaud <srenaud@42lausanne.ch>            #+#  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 19:28:02 by srenaud           #+#    #+#             */
-/*   Updated: 2025/12/01 15:39:57 by srenaud        ########   odam.nl        */
+/*   Updated: 2026/01/16 15:15:46 by srenaud        ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ int rt_parse_cam(char *line, t_la_complete *la_complete)
 
 int	arg_all_is_valid(char **arg)
 {
-	if (!rt_parse_util_is_in_range(arg[3],"0","70"))
+	if (!rt_parse_util_is_in_range(arg[3],"-1234","1234"))
 	{
 		printf("%s\n", arg[3]);
 		return (rt_error_put(ERROR_OUT_OF_RANGE), 1);
--- src/parsing/rt_parse_util_is_in_range.c
+++ src/parsing/rt_parse_util_is_in_range.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rt_parse_util_is_in_range.c                        :+:      :+:    :+:   */
+/*   rt_parse_util_is_in_range.c                         :+:    :+:           */
 /*                                                    +:+ +:+         +:+     */
 /*   By: simon2314 <simon2314@42lausanne.ch>        #+#  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/29 15:13:09 by simon2314         #+#    #+#             */
-/*   Updated: 2025/12/29 15:13:09 by simon2314        ###   ####lausanne.ch   */
+/*   Updated: 2026/01/16 19:24:23 by srenaud        ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,30 @@
 int	is_higher(char *value, char *limit);
 int	dot_handle(char *a, char *b);
 int	nega_handle(char *a, char *b);
+int	is_only_zero(char *str);
 
 /**
- * @brief	check if the assci argument is in the range
+ * @brief	check if the assci argument is in the range comparing caracteres
+ *			the value can be negative, enter and rationnal
+ * @param	value
+ * @param	min
+ * @param	max
+ * @fonctionnnement the value 
  * @return	1 if it's in range, 0 ifn't
  *
 */
 
 int	rt_parse_util_is_in_range(char *value, char *min, char *max)
 {
-	if (!is_higher(min, value) && !is_higher(value, max))
+	if (is_higher(value, min) && is_higher(max, value))
 		return (1);
 	return (0);
 }
+
+/**
+ * @brief	check if the ascii value of the first parameter is stricly bigger
+ *			than the second
+*/
 
 int	is_higher(char *a, char *b)
 {
@@ -37,14 +48,16 @@ int	is_higher(char *a, char *b)
 	{
 		if (*a < *b)
 			return (0);
+		if (*a > *b)
+			return (1);
 		a++;
 		b++;
 	}
 	if (*a == '.' || *b == '.')
 		return (dot_handle(++a, ++b));
-	if (ft_isdigit(*a)) 
-		return (1);
-	return (0);
+	if (ft_isdigit(*b)) // for a case like a = 123, b = 12  
+		return (0);
+	return (1);
 }
 
 int	nega_handle(char *a, char *b)
@@ -67,6 +80,10 @@ int	nega_handle(char *a, char *b)
 
 int	dot_handle(char *a, char *b)
 {
+	int	a_is_zero;
+	int b_is_zero;
+
+
 	if (ft_isdigit(*a) && *b == '.')
 		return (1);
 	if (*a == '.' && *b == '.')
@@ -74,3 +91,12 @@ int	dot_handle(char *a, char *b)
 	return (0);
 }
 
+int	is_only_zero(char *str)
+{
+	while (*str)
+	{
+		if (*str != '0')
+			return(0);
+	}
+	return (1);
+}
